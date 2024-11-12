debeb0f1dca530800f4e9ff5bba0fe9d66a2f66f
Razia <sidramehvish23@gmail.com>
2024-11-12 16:05:46 +0530

    learing

--- 3-vite-project/src/components/Para.jsx
+++ 3-vite-project/src/components/Para.jsx
@@ -1,7 +1,7 @@
 
 
-function Paragraph(){
+const Paragraph = ()=>{
   return 
-  <p>razia</p>
+  <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eligendi a molestiae dolorum, deserunt ipsam quam fuga facere, esse iure hic consequuntur aliquid eaque eum sed aspernatur voluptatum illo excepturi animi?</p>;
 }
 export default Paragraph;
